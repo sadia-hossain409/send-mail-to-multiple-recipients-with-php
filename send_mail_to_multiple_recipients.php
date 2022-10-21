cd1ff019410972fc6e6306341ea8a49a5bb8631d
Sadia Hossain Khan <113246088+sadia-hossain409@users.noreply.github.com>
2022-10-21 04:44:11 -1200

    Add files via upload

---
+++ send_mail_to_multiple_recipients.php
@@ -0,0 +1,29 @@
+<?php
+ 
+$to="sadi@gmail.com";
+$sub="testing2";
+$msg="Dear T,\n
+How r u?"
+<body>
+
+
+
+
+
+
+
+
+
+
+
+$from="From:sadi@gmail.com" ."r\n". "CC:sad1@gmail.com";
+
+if(mail($to,$sub,$msg,$from)){
+echo "msg sent"	;
+	
+}
+else{
+	
+	echo "not sent";
+}
+?>
